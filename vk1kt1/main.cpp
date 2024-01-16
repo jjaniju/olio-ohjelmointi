9c7215c2dee58d78c5df1c4a2dfa4defcc2de1a3
jjaniju <152972331+jjaniju@users.noreply.github.com>
2024-01-16 13:53:51 +0200

    satunnaisluku tehtävän viimeisin versio.

---
+++ vk1kt1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+int leikki(int maxluku);
+
+using namespace std;
+
+int main() {
+    int maxluku;
+    cout<<"maxluku"<<endl;
+    cin>>maxluku;
+    srand(time(0)); // käytetään kelloa siemenlukuna.
+    int arvaukset = leikki(maxluku);
+    cout<<"arvauksia oli "<<arvaukset<<endl;
+
+}
+
+int leikki(int maxluku ){
+
+
+    int luku = rand() % maxluku ;
+    int arvaus;
+    int quesses = 1;
+    do {
+        cout << "Arvaa luku 1-"<<maxluku<<endl;
+        cin >> arvaus;
+
+        if (arvaus < luku) {
+            cout << "isompi" << endl;
+            quesses++;
+        } else if (arvaus > luku) {
+            cout << "pienempi" << endl;
+            quesses++;
+        } else {
+            cout << "oikein!" << endl;
+        }
+    } while (arvaus != luku);
+    return quesses;
+}
