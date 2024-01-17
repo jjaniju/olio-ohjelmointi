bb4848831c2009b4d9e8731303cae8a1756ce095
jjaniju <152972331+jjaniju@users.noreply.github.com>
2024-01-17 19:38:57 +0200

    viikon 2 kotitehtävä aloitettu

---
+++ viikko2/vk2kt1/main.cpp
@@ -0,0 +1,43 @@
+#include "game.h"
+using namespace std;
+
+
+
+
+
+
+class game
+{
+public:
+     ~game();
+    int Game;
+     void play();
+
+
+private:
+
+    int maxNumber;
+    int playersGuess;
+    int randomNumber;
+    int numOfGuesses;
+    void printGameResult();
+
+
+
+int main()
+{
+    game gameObject (10);
+    gameObject.play();
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
